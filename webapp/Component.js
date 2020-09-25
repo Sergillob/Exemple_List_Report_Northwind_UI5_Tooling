dfa4575b47ea482c7a9548d67d45dfbca38ba864
Llobet Mas <sergi.llobet@atos.net>
2020-09-25 17:44:14 +0200

    Primer Commit desde VScode

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("sl.elements.exemple_list_report_northwind_ui5_tooling.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
